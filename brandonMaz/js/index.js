a3dd8e377f8ed08800038910e88b4a13819273ed
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-02-28 20:00:21 -0800

    made changes to layout and added new hamburger

--- brandonMaz/js/index.js
+++ brandonMaz/js/index.js
@@ -1,9 +1,22 @@
-const navToggle = document.querySelector('.nav-toggle')
-const navLinks = document.querySelectorAll('.nav_link')
+// const navToggle = document.querySelector('.nav-toggle')
+// const navLinks = document.querySelectorAll('.nav_link')
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open')
-})
+// navToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open')
+// })
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         document.body.classList.remove('nav-open')
+//     })
+// })
+const navWrapper = document.querySelector('.nav-wrapper')
+const navLinks = document.querySelectorAll('.nav_link')
+const navToggler =  document.querySelector('.nav-toggler')
+
+		navToggler.addEventListener('click', function (event) {
+			navWrapper.classList.toggle('active')
+		})
+
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open')
