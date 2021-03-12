bafe3abd97ec472169ad3a39046372bcc286138b
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-03-12 11:04:33 -0800

    made some corrections on the mobile site again adn again

--- brandonMaz/js/index.js
+++ brandonMaz/js/index.js
@@ -13,9 +13,9 @@ const navWrapper = document.querySelector('.nav-wrapper')
 const navLinks = document.querySelectorAll('.nav_link')
 const navToggler =  document.querySelector('.nav-toggler')
 
-		navToggler.addEventListener('click', function (event) {
-			navWrapper.classList.toggle('active')
-		})
+navToggler.addEventListener('click', function (event) {
+    navWrapper.classList.toggle('active')
+})
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
